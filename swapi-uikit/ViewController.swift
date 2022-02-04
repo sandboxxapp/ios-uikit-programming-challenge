e733eca1ac033bf1b77970717dafd8bb029ec2d4
hbanzon <hino.banzon@gmail.com>
2022-02-04 10:47:01 -0500

    Initial Commit

---
+++ swapi-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swapi-uikit
+//
+//  Created by Hino Banzon on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
