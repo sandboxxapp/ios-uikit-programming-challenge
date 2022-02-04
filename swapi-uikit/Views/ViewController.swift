f6dd0485b3c99d14179242f76969a5ca6afe1d86
hbanzon <hino.banzon@gmail.com>
2022-02-04 14:32:28 -0500

    Update ViewController.swift

--- swapi-uikit/Views/ViewController.swift
+++ swapi-uikit/Views/ViewController.swift
@@ -34,8 +34,18 @@ class ViewController: UIViewController {
 // MARK: UITableViewDataSource
 
 extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data?.results.count ?? 0
+  }
+  
   func tableView(_ tableView: UITableView,
                  cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as? TableViewCell,
+          let vehicle = data?.results[indexPath.row] else {
+      return UITableViewCell()
+    }
     
+    cell.set(vehicle: vehicle)
+    return cell
   }
 }
